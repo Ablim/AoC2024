ef8f3f2f2c9c9bf0fb55330fc63bb3d6f48ae930
Albin Bramstång <albin.bramstang@devies.se>
2024-11-19 17:01:21 +0100

    Prep

---
+++ AoC2024/Day1/Solution.fs
@@ -0,0 +1,7 @@
+module AoC2024.Day1.Solution
+
+let solve1 =
+    "1"
+    
+let solve2 =
+    "2"
---
+++ AoC2024/Main/Program.fs
@@ -0,0 +1,19 @@
+﻿module Program
+
+open AoC2024.Day1.Solution
+
+[<EntryPoint>]
+let main args =
+    let day = 1
+    printfn $"Advent of Code - Day {day}"
+    printfn ""
+    
+    let solution1 = solve1
+    printfn "Part 1:"
+    printfn $"{solution1}"
+    printfn ""
+    
+    let solution2 = solve2
+    printfn "Part 2:"
+    printfn $"{solution2}"
+    0
