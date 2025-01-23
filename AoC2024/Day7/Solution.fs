a7a8db700d460b5bb47561216312988ad53415db
Albin Bramstång <albin.bramstang@devies.se>
2025-01-23 10:12:49 +0100

    Day 7 part 2

--- AoC2024/Day7/Solution.fs
+++ AoC2024/Day7/Solution.fs
@@ -16,7 +16,9 @@ let rec calculate2 (answer: bigint) (maybeAnswer: bigint) numbers =
         if maybeAnswer > answer then
             false
         else
-            calculate2 answer (h + maybeAnswer) t || calculate2 answer (h * maybeAnswer) t
+            calculate2 answer (maybeAnswer + h) t
+            || calculate2 answer (maybeAnswer * h) t
+            || calculate2 answer ($"{maybeAnswer}{h}" |> bigint.Parse) t
 
 let solve1 (puzzleInput: string seq) =
     let answers =
@@ -56,7 +58,7 @@ let solve2 (puzzleInput: string seq) =
     let result =
         seq {
             for i in 0..answers.Length - 1 do
-                if calculate2 answers[i] 0I terms[i] then
+                if calculate2 answers[i] terms[i].Head terms[i].Tail then
                     answers[i]
         }
         |> Seq.sum
