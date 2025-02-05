a48dd56a9294207c4895980ded2d0584839b8b48
Albin Bramstång <albin.bramstang@devies.se>
2025-02-05 21:27:00 +0100

    Day 8 part 2

--- AoC2024/Day8/Solution.fs
+++ AoC2024/Day8/Solution.fs
@@ -30,20 +30,33 @@ let getAntiNodes (map: char array array) =
     |> List.distinct
     |> List.filter (fun (r, c) -> r >= 0 && r < map.Length && c >= 0 && c < map[0].Length)
     
-let getAntiNodesBetween2 rowA colA rowB colB lastRow lastCol =
-    let deltaRow = rowA - rowB
-    let deltaCol = colA - colB
-    let rowA' = rowA + deltaRow
-    let rowB' = rowB - deltaRow
-    let colA' = colA + deltaCol
-    let colB' = colB - deltaCol
-    [ (rowA', colA'); (rowB', colB') ]
+let getAntiNodesBetween2 antenna1 antenna2 height width =
+    // from a1 through a2 until the limit, get all nodes
+    let row1, col1, _ = antenna1
+    let row2, col2, _ = antenna2
+    let deltaRow = row2 - row1
+    let deltaCol = col2 - col1
+    let mutable factor = 0
+    
+    seq {
+        while row1 + deltaRow * factor >= 0
+            && row1 + deltaRow * factor < height
+            && col1 + deltaCol * factor >= 0
+            && col1 + deltaCol * factor < width do
+            
+            let temp = factor
+            factor <- factor + 1
+            (row1 + deltaRow * temp, col1 + deltaCol * temp)
+    }
+    |> Seq.toList
 
 let getAntiNodes2 (map: char array array) =
+    let height = map.Length
+    let width = map[0].Length
     let antennas =
         seq {
-            for row in 0..map.Length - 1 do
-                for col in 0..map[0].Length - 1 do
+            for row in 0..(height - 1) do
+                for col in 0..(width - 1) do
                     if map[row][col] <> '.' then
                         (row, col, map[row][col])
         }
@@ -51,15 +64,14 @@ let getAntiNodes2 (map: char array array) =
     seq {
         for a1 in antennas do
             for a2 in antennas do
-                let row1, col1, freq1 = a1
-                let row2, col2, freq2 = a2
+                let _, _, freq1 = a1
+                let _, _, freq2 = a2
                 
                 if a1 <> a2 && freq1 = freq2 then
-                    getAntiNodesBetween2 row1 col1 row2 col2 (map.Length - 1) (map[0].Length - 1)
+                    getAntiNodesBetween2 a1 a2 height width
     }
     |> List.concat
     |> List.distinct
-    |> List.filter (fun (r, c) -> r >= 0 && r < map.Length && c >= 0 && c < map[0].Length)
 
 let solve1 (puzzleInput: string seq) =
     let antennas =
